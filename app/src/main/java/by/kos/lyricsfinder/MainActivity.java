193827e498514204e55afe2f13a4ea8dea8eaf26
Eugene <mstrsid@live.com>
2020-08-27 14:14:33 +0300

    fix bugs, add hide keyboard on outside touch

--- app/src/main/java/by/kos/lyricsfinder/MainActivity.java
+++ app/src/main/java/by/kos/lyricsfinder/MainActivity.java
@@ -2,8 +2,12 @@ package by.kos.lyricsfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -29,6 +33,27 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewLyrics;
     //TODO: find api with capability russian artists
 
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View v = getCurrentFocus();
+
+        if (v != null && (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) &&
+                v instanceof EditText &&
+                !v.getClass().getName().startsWith("android.webkit.")) {
+            int[] sourceCoordinates = new int[2];
+            v.getLocationOnScreen(sourceCoordinates);
+            float x = ev.getRawX() + v.getLeft() - sourceCoordinates[0];
+            float y = ev.getRawY() + v.getTop() - sourceCoordinates[1];
+
+            if (x < v.getLeft() || x > v.getRight() || y < v.getTop() || y > v.getBottom()) {
+                hideKeyboard(this);
+            }
+
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
         editTextSong = findViewById(R.id.editTextSong);
         buttonSearch = findViewById(R.id.buttonSearch);
         textViewLyrics = findViewById(R.id.textViewLyrics);
+        findViewById(android.R.id.content).setFocusableInTouchMode(true);
 
         buttonSearch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,12 +71,13 @@ public class MainActivity extends AppCompatActivity {
                     String songName = editTextSong.getText().toString();
                     String url = String.format("https://api.lyrics.ovh/v1/%s/%s", artist, songName);
                     url.replace(" ", "20%");
+                    textViewLyrics.setText("");
+
                     RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
                     JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(JSONObject response) {
                             try {
-                                textViewLyrics.setText("");
                                 textViewLyrics.setText(response.getString("lyrics"));
                             } catch (JSONException e) {
                                 e.printStackTrace();
@@ -66,5 +93,17 @@ public class MainActivity extends AppCompatActivity {
             requestQueue.add(jsonObjectRequest);
             }
         });
+
+
+    }
+    private void hideKeyboard(Activity activity) {
+        if (activity != null && activity.getWindow() != null) {
+            activity.getWindow().getDecorView();
+            InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm != null) {
+                imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(), 0);
+            }
+        }
+        findViewById(android.R.id.content).clearFocus();
     }
 }
